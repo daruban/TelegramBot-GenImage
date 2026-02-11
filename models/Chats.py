349651dd204686e36c731f7f3fe374782fdb090f
RomanovaV <v.romanova@t1-group.ru>
2026-02-11 14:03:44 +0300

    up models

--- models/Chats.py
+++ models/Chats.py
@@ -2,13 +2,11 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime, JSON
 from datetime import datetime
 from models import Base
 
-
-# Модель = Python-класс = таблица в БД
 class Chat(Base):
-    __tablename__ = "chats"  # Имя таблицы
+    __tablename__ = 'chats'
     id = Column(Integer, primary_key=True)
     chat_id = Column(Integer, unique=True, nullable=False, index=True)
     chat_data = Column(JSON, nullable=False)
-    is_active = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=False) 
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
