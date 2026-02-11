e70bba3d1797cff8ce79c917a7f49ba1a4e8667e
RomanovaV <v.romanova@t1-group.ru>
2026-02-11 14:04:15 +0300

    up env

--- config/env.py
+++ config/env.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные из .env файла
 load_dotenv()
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 STABLE_DIFFUSION_URL = os.environ["STABLE_DIFFUSION_URL"]
 DB_URI = os.environ["DB_URI"]
+
+
